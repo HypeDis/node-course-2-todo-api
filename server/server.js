8ab72f9af4bd9fa71dc11b53f17478d4c314a299
unknown <m.bae@live.com>
2018-05-25 18:53:35 -0700

    Add get/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const {mongoose} = require ('./db/mongoose.js');
-const {Todo} = require ('./models/todo.js');
-const {Users} = require ('./models/users.js');
+const { ObjectID } = require('mongodb');
+const { mongoose } = require('./db/mongoose.js');
+const { Todo } = require('./models/todo.js');
+const { Users } = require('./models/users.js');
 
 let app = express();
 
@@ -14,27 +15,53 @@ app.post('/todos', (req, res) => {
         text: req.body.text
     });
 
-    todo.save().then((doc) =>{
+    todo.save().then((doc) => {
         res.send(doc);
-        
-    },(e) => {
+
+    }, (e) => {
         // console.log(res.status);
         res.status(400).send(e);
     });
-}); 
+});
 
 
-app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
-        res.send({todos});
+// app.get('/todos', (req, res) => {
+//     Todo.find().then((todos) => {
+//         res.send({ todos });
+//     }, (e) => {
+//         res.status(400).send(e);
+//     });
+
+// });
+
+// get /todos/12342134
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('404 id is invalid');
+    }
+    Todo.findById(id).then((todo) => {
+        if (todo) {
+            res.status(200).send({ todo });
+        } else {
+            res.status(404).send('id does not exist');
+        }
     }, (e) => {
-        res.status(400).send(e);
+        res.status(400).send();
     });
+    //validate id
+    //404 -send back 
 
+    //findbyid
+    //success
+    //if todo send it back
+    //if no todo send back 404 empty body 
+    //error case
+    //send back 400 - and send empty body
 });
 
 app.listen(3000, () => {
     console.log('Started on port 3000')
 });
 
-module.exports = {app};
+module.exports = { app };
