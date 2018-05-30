d3e12593a213ba8507ca3533507d214ee899ab72
unknown <m.bae@live.com>
2018-05-30 01:06:26 -0700

    add update request to todo app

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 const { ObjectID } = require('mongodb');
 const { mongoose } = require('./db/mongoose.js');
@@ -80,6 +81,32 @@ app.delete('/todos/:id', (req, res) => {
 });
 app.listen(port, () => {
     console.log('Started on port port', port);
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('404 id is invalid');
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({ todo });
+    }).catch((e) => {
+        res.status(400).send();
+    });
 });
 
 module.exports = { app };
